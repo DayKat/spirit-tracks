155a3449dcb9ae7419581aee72b8cf288ccd927f
carrotinator <chrisb1820@gmail.com>
2025-11-21 20:08:47 +0100

    spirit-tracks: added instructions to stage flags

--- worlds/tloz_st/data/Constants.py
+++ worlds/tloz_st/data/Constants.py
@@ -58,6 +58,9 @@ STARTING_FLAGS = [
     [0x26901E, 0, 0]
 ]
 
+# You can find the stage flags for a stage by checking the stage data pointer of 0x265164 and adding an offset of 176 (note decimal) to it's value
+# then endian is opposite of what it usually is cause i like to use spreadsheets to import it.
+# check the stage flag page in the spreadsheet to see what each bit corresponds to.
 STAGE_FLAGS = {
 
     0x04: [0x02, 0x00, 0x00, 0x00], # Forest Realm
